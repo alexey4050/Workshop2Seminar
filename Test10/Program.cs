159f4cca6df702e5f249ce977ff52947ed3dbd0d
alexey <alexey4050@mail.ru>
2022-08-10 20:28:48 +0300

    Решение задачи номер 10 (два варанта решения задачи)

---
+++ Test10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+// Первый вариант решения задачи 10.
+Console.WriteLine("Введите целое трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = number / 10;
+number = number % 10;
+Console.WriteLine($"Вторая цифра трехзначного числа: {number}");
+
+// Второй вариант решения задачи 10.
+Console.WriteLine("Введите целое трехзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+{
+    Console.WriteLine($"Вторая цифра трехзначного числа -> " +stringNumber[1]);
+}
